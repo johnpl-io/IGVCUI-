de810b4942df46e0ddbf2db572350de06c96b177
giuseppequaratino <71469267+giuseppequaratino@users.noreply.github.com>
2021-01-31 17:51:32 -0500

    added test ffi library

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http')
+/*const http = require('http')
 const fs = require('fs')
 const port = 8000
 const zoomooz = require('zoomooz')
@@ -25,4 +25,38 @@ if (error) {
 else {
 console.log('Server is listening on port ' + port)
 }
-})
+}) */
+
+
+const msg1 = "test";
+console.log(msg1);
+
+const ffi = require('ffi-napi');
+
+const msg2 = "test";
+console.log(msg2);
+
+// Converts JSString to CString
+function TEXT(text) {
+  return Buffer.from(`${text}\0`, "ucs2"); 
+} 
+
+// Import user32 library
+const user32 = new ffi.Library("user32", {
+  "MessageBoxW": [
+    "int32", ["int32", "string", "string", "int32"]
+  ],
+  "SetCursorPos": [
+    "bool", ["int32", "int32"]
+  ]
+});
+
+// Call function
+const OK_or_Cancel = user32.MessageBoxW(
+  0, TEXT("Hellow from Node.js!"), TEXT("Hellow World!"), 1
+)
+
+console.log(OK_or_Cancel);
+
+user32.SetCursorPos(0, 0); 
+
